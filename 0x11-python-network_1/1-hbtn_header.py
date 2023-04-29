76aec4e5365a2d7fa5c6e9a0f3550e284ecc4be2
Phumzi1e <Phumzilemiec@gmail.com>
2023-04-29 21:27:10 +0000

    task

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,10 @@
+cript that takes in a URL, sends a request and display the value
+in the X-Request-Id variable found in the header"""
+
+if __name__ == '__main__':
+    import sys
+    import urllib.request
+    url = sys.argv[1]
+    with urllib.request.urlopen(url) as response:
+        url_res = response.info()
+        print(url_res['X-Request-Id'])
---
+++ 0x11-python-network_1/README.md
@@ -0,0 +1,1 @@
+8-json_api.py
